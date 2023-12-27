57bac259e6971479a88c7a868a173f64bbd67fef
codec.js <144981186+vpm0001@users.noreply.github.com>
2023-12-27 16:40:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$('.button').click((e)=>{
+	if($('.button').is('.button-state')){
+		console.log('clicked');
+		$('.button').removeClass('button-state').addClass('card-state');
+	}
+});
+$('.signin-button').click((e)=>{
+	$('.button').addClass('button-state').removeClass('card-state');
+	e.stopPropagation();
+});
